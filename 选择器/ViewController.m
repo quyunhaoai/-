186da0b2fdf10400556c6bcc37c18019db8c3823
quyunhaoai <quyunhaoai@163.com>
2018-02-28 10:56:05 +0800

    UIDatePicker

--- 选择器/ViewController.m
+++ 选择器/ViewController.m
@@ -7,16 +7,41 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "ZHPickView.h"
+@interface ViewController ()<ZHPickViewDelegate>
+@property (nonatomic,strong)ZHPickView *pickview;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+}
+- (IBAction)buttonAction:(id)sender {
+    
+    
+    [self addZhpickview];
+    
+    
+}
+
+-(void)addZhpickview
+{
+    NSArray *array = @[@[@"1",@"大幅度",@" 大师傅",@" 阿道夫爱打"],@[@"2",@" 爱打发",@" 的萨芬爱上",@" 爱打发的"],@[@" 大师傅",@" 的萨芬爱上",@"阿斯顿发生",@" 爱打飞 "]];
+    self.pickview = [[ZHPickView alloc]initPickviewWithArray:array isHaveNavControler:NO];
+//    [self.pickview setBackgroundColor:[UIColor redColor]];
+    self.pickview.delegate = self;
+    [self.view addSubview:self.pickview];
+}
+
+#pragma mark ZHpickviewDelet
+-(void)toobarDonBtnHaveClick:(ZHPickView *)pickView resultString:(NSString *)resultString
+{
+    self.disLablel.text = resultString;
 }
 
 - (void)didReceiveMemoryWarning {
